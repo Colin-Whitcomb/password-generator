dad0a34def4cb017bea06750ea8df9cc977a628a
Colin Whitcomb <colinsw@berkeley.edu>
2020-06-17 19:30:27 -0700

    Worked through generating password conditional statements.

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,36 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var criteria = infoGrab();
-  var password = generatePassword();
+  var password = generatePassword(criteria);
   var passwordText = document.querySelector("#password");
   passwordText.value = password; 
 }
 
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(criteria) {
+  var generatedCh = [];
+
+if (criteria.uppercase === true){
+  optionSourcses.functionArray = optionSources.functionArray.concat(criteria.uppercaseLetters);
+
+}
+if (criteria.lowercase === true){
+  optionSources.functionArray = optionSources.functionArray.concat(criteria.lowercaseLetters);
+}
+if (criteria.numbers === true){
+  optionSources.functionArray = optionSources.functionArray.concat(criteria.numbers);
+}
+if (criteria.special === true) {
+  optionSources.functionArray = optionSources.functionArray.concat(criteria.specialCharacters);
+}
+for (var i = 0; i < criteria.length; i++) {
+  generatedCh.push(generateRandomVariables(criteria.functionArray));
+}
+return generatedCh;
+}
+
+
 
 //Global Variables
 // var result = '';
@@ -17,11 +41,11 @@ generateBtn.addEventListener("click", writePassword);
 
 // Object "optionSources." Arrays listed within. Function array will support all pushed arrays. 
 var optionSources = {
-  "functionArray": [" "],
-  "uppercaseLetters": ["A", "B", "C", "D", "E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",],
-  "lowercaseLetters": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",],
-  "numbers": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  "specialCharacters": ["\\", ".", "+", "*", "?", "[", "^", "]", "$", "(", ")", "{", "}", "=", "!", "<", ">", "|", ":", "-", ],
+  functionArray: [],
+  uppercaseLetters: ["A", "B", "C", "D", "E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",],
+  lowercaseLetters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",],
+  numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  specialCharacters: ["\\", ".", "+", "*", "?", "[", "^", "]", "$", "(", ")", "{", "}", "=", "!", "<", ">", "|", ":", "-", ],
 
 };
 
@@ -45,14 +69,6 @@ function infoGrab(){
       alert("Passwords much be between 8-128 Characters");
       return;
     }
-
-    // else if (lengthResponse > 129){
-    //   alert("Passwords much be between 8-128 Characters");
-    // }
-
-    // else if (lengthResponse === null){
-    //   alert("Passwords much be between 8-128 Characters");
-    // }
 
       var uppercaseResponse = confirm("Would you like to use uppercase letters?");
 
@@ -78,8 +94,24 @@ function infoGrab(){
       return collectedResponse;
     }
 
-    
+    function generatePassword(criteria) {
+      var pulledReturn = criteria
+    }
 
+    function generateRandomVariables(array) {
+      var result = Math.floor(Math.random() * array.length);
+      var collectedCharacter = array[result];
+      return collectedCharacter;
+    }
+
+    function generateRandomVariables() {
+      for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+
+
+ // for (var i = 0; i < array.length; i++)
 // for (var i = 0; i < questions.length; i++) {
 //   objectWithQuestions.questions.forEach(myFunction);
 //   var userResponse = confirm([i]);
@@ -107,8 +139,13 @@ function infoGrab(){
   // }
 
 // Creates random number and letter combination of 12 integers
-// for (var i = 0; i < length; i++) {
-//   result += characters.charAt(Math.floor(Math.random() * charactersLength));// Add +1??
+
+// function generateRandomVariables() {
+//   for (var i = 0; i < length; i++) {
+//   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+// }
+
+// Add +1??
 //   console.log(makeid(12)); // Operator is 12 right now ... need to add function that connects it to the 8-128 choice.
 // }
 // return result;
